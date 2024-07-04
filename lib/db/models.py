cf1f10496b2227e3323a91ea941f2267fc9ffa95
Elijah Alaak <alaakelijah78@gmail.com>
2024-07-04 07:50:09 +0300

    completed

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,1 +1,19 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 
+engine = create_engine('sqlite:///phonebook.db', echo=False)
+Base = declarative_base()
+
+class Person(Base):
+__tablename__ = 'person'
+id = Column(Integer, primary_key=True)
+name = Column(String)
+
+phones = relationship('Phone', back_populates='person')
+addresses = relationship('Address', back_populates='person')
+
+def __repr__(self):
+return f'<Person(id={self.id}, name={self.name})>'
+
+class Phone(Base):
