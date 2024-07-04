501a9dc5e5526cda15ca30684d2a35537d90a822
Elijah Alaak <alaakelijah78@gmail.com>
2024-07-04 07:45:18 +0300

    pending completion

--- lib/helpers.py
+++ lib/helpers.py
@@ -13,3 +13,20 @@ for address in person.addresses:
 print(f" - {address.street}, {address.city}, {address.state}")
 else:
 print(f"No person found with name '{name}'.")
+
+def add_new_person(name):
+person = Person(name=name)
+session.add(person)
+session.commit()
+print(f"Added new person '{name}' to the phonebook.")
+
+def add_phone_to_person(person_id, number, phone_type):
+person = session.query(Person).filter_by(id=person_id).first()
+if person:
+phone = Phone(number=number, type=phone_type)
+person.phones.append(phone)
+session.commit()
+print(f"Added phone number '{number}' ({phone_type}) to person '{person.name}'.")
+else:
+print(f"Person with ID '{person_id}' not found.")
+
