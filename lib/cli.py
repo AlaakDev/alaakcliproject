8e62ffe3a3d5a36762b3775d39cb2ba70966b115
Elijah Alaak <alaakelijah78@gmail.com>
2024-07-04 08:49:42 +0300

    completed

--- lib/cli.py
+++ lib/cli.py
@@ -2,31 +2,30 @@ from helpers import search_person_by_name, add_new_person, add_phone_to_person
 import sys
 
 if __name__ == '__main__':
-print('Welcome to Phonebook CLI!')
+    print('Welcome to Phonebook CLI!')
 
-while True:
-print('\nMenu:')
-print('1. Search for a person by name')
-print('2. Add a new person to the phonebook')
-print('3. Add a phone number to an existing person')
-print('4. Exit\n')
+    while True:
+        print('\nMenu:')
+        print('1. Search for a person by name')
+        print('2. Add a new person to the phonebook')
+        print('3. Add a phone number to an existing person')
+        print('4. Exit\n')
 
-choice = input('Enter your choice: ')
+        choice = input('Enter your choice: ')
 
-if choice == '1':
-name = input('Enter the name to search: ')
-search_person_by_name(name)
-elif choice == '2':
-name = input('Enter the name of the new person: ')
-add_new_person(name)
-elif choice == '3':
-person_id = input('Enter the ID of the person: ')
-number = input('Enter the phone number: ')
-phone_type = input('Enter the type of phone number: ')
-add_phone_to_person(person_id, number, phone_type)
-elif choice == '4':
-
-print('Exiting Phonebook CLI.')
-break
-else:
-print('Invalid choice. Please try again.')
+        if choice == '1':
+            name = input('Enter the name to search: ')
+            search_person_by_name(name)
+        elif choice == '2':
+            name = input('Enter the name of the new person: ')
+            add_new_person(name)
+        elif choice == '3':
+            person_id = input('Enter the ID of the person: ')
+            number = input('Enter the phone number: ')
+            phone_type = input('Enter the type of phone number: ')
+            add_phone_to_person(person_id, number, phone_type)
+        elif choice == '4':
+            print('Exiting Phonebook CLI.')
+            break
+        else:
+            print('Invalid choice. Please try again.')
