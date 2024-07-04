05881cec9636c55082425729be54b5ba5fc6445b
Elijah Alaak <alaakelijah78@gmail.com>
2024-07-04 07:51:11 +0300

    completed

--- lib/db/models.py
+++ lib/db/models.py
@@ -17,3 +17,33 @@ def __repr__(self):
 return f'<Person(id={self.id}, name={self.name})>'
 
 class Phone(Base):
+    __tablename__ = 'phone'
+id = Column(Integer, primary_key=True)
+number = Column(String)
+type = Column(String)
+person_id = Column(Integer, ForeignKey('person.id'))
+
+person = relationship('Person', back_populates='phones')
+
+def __repr__(self):
+return f'<Phone(id={self.id}, number={self.number}, type={self.type})>'
+
+class Address(Base):
+__tablename__ = 'address'
+id = Column(Integer, primary_key=True)
+street = Column(String)
+city = Column(String)
+state = Column(String)
+person_id = Column(Integer, ForeignKey('person.id'))
+
+person = relationship('Person', back_populates='addresses')
+
+def __repr__(self):
+return f'<Address(id={self.id}, street={self.street}, city={self.city}, state={self.state})>'
+
+# Create the tables in the database
+Base.metadata.create_all(engine)
+
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
