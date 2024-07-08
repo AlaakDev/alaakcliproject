2fe0059d07c7d29cb9e7615785f245cc20e54311
Elijah Alaak <alaakelijah78@gmail.com>
2024-07-08 07:53:21 +0300

    completed

--- lib/cli.py
+++ lib/cli.py
@@ -1,43 +1,41 @@
-import click
 from helpers import search_person_by_name, add_new_person, add_phone_to_person, delete_person_by_id, list_all_persons
-
-@click.group()
-def cli():
-    """Welcome to Phonebook CLI!"""
-    pass
-
-@cli.command()
-@click.argument('name')
-def search(name):
-    """Search for a person by name"""
-    search_person_by_name(name)
-
-@cli.command()
-@click.argument('name')
-def add(name):
-    """Add a new person to the phonebook"""
-    add_new_person(name)
-
-@cli.command()
-@click.argument('person_id')
-@click.argument('number')
-@click.argument('phone_type')
-def add_phone(person_id, number, phone_type):
-    """Add a phone number to an existing person"""
-    add_phone_to_person(person_id, number, phone_type)
-
-@cli.command()
-@click.argument('person_id')
-def delete(person_id):
-    """Delete a person from the phonebook"""
-    delete_person_by_id(person_id)
-
-@cli.command()
-def list_all():
-    """List all persons in the phonebook"""
-    list_all_persons()
+import sys
 
 if __name__ == '__main__':
-    cli()
+    print('Welcome to Phonebook CLI!')
+
+    while True:
+        print('\nMenu:')
+        print('1. Search for a person by name')
+        print('2. Add a new person to the phonebook')
+        print('3. Add a phone number to an existing person')
+        print('4. Delete a person from the phonebook')
+        print('5. List all persons in the phonebook')
+        print('6. Exit\n')
+
+        choice = input('Enter your choice: ')
+
+        if choice == '1':
+            name = input('Enter the name to search: ')
+            search_person_by_name(name)
+        elif choice == '2':
+            name = input('Enter the name of the new person: ')
+            add_new_person(name)
+        elif choice == '3':
+            person_id = input('Enter the ID of the person: ')
+            number = input('Enter the phone number: ')
+            phone_type = input('Enter the type of phone number: ')
+            add_phone_to_person(person_id, number, phone_type)
+        elif choice == '4':
+            person_id = input('Enter the ID of the person to delete: ')
+            delete_person_by_id(person_id)
+        elif choice == '5':
+            list_all_persons()
+        elif choice == '6':
+            print('Exiting Phonebook CLI.')
+            break
+        else:
+            print('Invalid choice. Please try again.')
+
 
 
